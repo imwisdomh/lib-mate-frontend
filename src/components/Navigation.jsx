dd67504c0116f60b1650b8fb651cd1191203f095
JH <imwisdomh@gmail.com>
2024-12-27 14:39:22 +0900

    style: Navigation 스타일 업데이트

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,53 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  background-color: rgb(248, 249, 250);
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const NavList = styled.ul`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const NavItem = styled.li`
+  margin: 0 1rem;
+`;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: rgb(18, 124, 107);
+  font-weight: bold;
+
+  &:hover {
+    color: rgb(98, 190, 46);
+    text-decoration: underline;
+  }
+
+  &:active {
+    color: rgb(204, 209, 18);
+  }
+`;
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/board">Board</Link>
-        </li>
-        <li>
-          <Link to="/seat-management">Seat Management</Link>
-        </li>
-      </ul>
-    </nav>
+    <Nav>
+      <NavList>
+        <NavItem>
+          <NavLink to="/home">Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/seat-management">Seat Management</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/board">Board</NavLink>
+        </NavItem>
+      </NavList>
+    </Nav>
   );
 };
 
