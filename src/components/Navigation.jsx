93c8a1d012944c324b7c24a937c579fdb58124c6
JH <imwisdomh@gmail.com>
2024-12-30 02:15:05 +0900

    style: Navigation 스타일 업데이트

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,27 +1,39 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faCommentNodes } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = styled.nav`
   background-color: rgb(248, 249, 250);
   position: fixed;
   bottom: 0;
   width: 100%;
-  box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.1);
 `;
 
 const NavList = styled.ul`
   display: flex;
-  justify-content: space-around;
 `;
 
 const NavItem = styled.li`
-  margin: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+const NavIcon = styled(FontAwesomeIcon)`
+  color: rgb(18, 124, 107);
+  font-size: 1.2rem;
+  margin-bottom: 5px;
 `;
 
 const NavLink = styled(Link)`
   text-decoration: none;
   color: rgb(18, 124, 107);
-  font-weight: bold;
+  font-weight: 500;
 
   &:hover {
     color: rgb(98, 190, 46);
@@ -33,18 +45,36 @@ const NavLink = styled(Link)`
   }
 `;
 
+const SeatManagementLink = styled(Link)`
+  text-decoration: none;
+  font-weight: 500;
+  color: white;
+  background: linear-gradient(45deg, rgb(18, 124, 107), rgb(98, 190, 46), rgb(204, 209, 18));
+  border-radius: 50%;
+  height: 60px;
+  width: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  top: -10px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1);
+`;
+
 const Navigation = () => {
   return (
     <Nav>
       <NavList>
         <NavItem>
-          <NavLink to="/home">Home</NavLink>
+          <NavIcon icon={faHouse} />
+          <NavLink to="/home">홈</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/seat-management">Seat Management</NavLink>
+          <SeatManagementLink to="/seat-management">내 좌석</SeatManagementLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/board">Board</NavLink>
+          <NavIcon icon={faCommentNodes} />
+          <NavLink to="/board">게시판</NavLink>
         </NavItem>
       </NavList>
     </Nav>
